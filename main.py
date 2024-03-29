aff01026d9f9b1fc005c34d6d06bd479c3b2d9fd
Erfanh83 <ehamzei40@gmail.com>
2024-03-29 14:36:28 +0330

    Adding root of project

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#Packages
 from random import random
 import math
 
+#Root(Base) variables and prints
+choices = ("rock","paper","scissor")
+computer_choice = choices[math.floor(random() * 3)]
+
+print(f"--------- Welcome to our (Rock, Paper, Scissor) game ---------")
+
+ready = input("Are you ready to start (y/n) ? ")
